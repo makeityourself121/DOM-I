f3373792c1ad77fd6e1d91419323e279178a769b
Dharmik Savaliya <danny@Dharmiks-MacBook-Pro.local>
2021-07-05 16:29:30 -0700

    alomost done

--- js/index.js
+++ js/index.js
@@ -40,3 +40,59 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const head = document.querySelectorAll('nav a');
+head[0].textContent=siteContent["nav"]["nav-item-1"]
+head[1].textContent=siteContent["nav"]["nav-item-2"]
+head[2].textContent=siteContent["nav"]["nav-item-3"]
+head[3].textContent=siteContent["nav"]["nav-item-4"]
+head[4].textContent=siteContent["nav"]["nav-item-5"]
+head[5].textContent=siteContent["nav"]["nav-item-6"]
+
+const ctatext=document.querySelector('.cta-text h1');
+ctatext.textContent=siteContent["cta"]["h1"];
+ctatext.style.width="300px"
+ctatext.style.height="200px"
+const cta=document.querySelector('.cta-text button');
+cta.textContent=siteContent["cta"]["button"]
+const ctaimg=document.querySelector('#cta-img')
+ctaimg.src=siteContent['cta']['img-src']
+
+const topContent=document.querySelector('.top-content .text-content h4');
+topContent.textContent=siteContent["main-content"]["features-h4"];
+const topFeature=document.querySelector('.top-content .text-content p');
+topFeature.textContent=siteContent["main-content"]["features-content"];
+const topRight=document.querySelector('.text-content:nth-of-type(2) h4')
+topRight.textContent=siteContent["main-content"]["about-h4"];
+const topRightText=document.querySelector('.text-content:nth-of-type(2) p')
+topRightText.textContent=siteContent["main-content"]["about-content"]
+
+const middleimg=document.querySelector('.middle-img')
+middleimg.src=siteContent['main-content']['middle-img-src']
+
+const bottomContent=document.querySelector('.bottom-content .text-content h4')
+bottomContent.textContent=siteContent["main-content"]["services-h4"]
+const servicetext=document.querySelector('.bottom-content .text-content p')
+servicetext.textContent=siteContent["main-content"]["services-content"]
+const bottomProduct=document.querySelector('.bottom-content .text-content:nth-of-type(2) h4')
+bottomProduct.textContent=siteContent["main-content"]["product-h4"]
+const productText=document.querySelector('.bottom-content .text-content:nth-of-type(2) p')
+productText.textContent=siteContent["main-content"]["product-content"]
+const vision=document.querySelector('.bottom-content .text-content:nth-of-type(3) h4')
+vision.textContent=siteContent["main-content"]["vision-h4"]
+const visionText=document.querySelector('.bottom-content .text-content:nth-of-type(3) p')
+visionText.textContent=siteContent["main-content"]["vision-content"]
+
+const contactInfo=document.querySelector('.contact h4')
+contactInfo.textContent=siteContent["contact"]["contact-h4"]
+
+const address=document.querySelector('.contact p:nth-of-type(1)')
+address.textContent=siteContent["contact"]["address"]
+address.style.width="200px"
+const phone=document.querySelector('.contact p:nth-of-type(2)')
+phone.textContent=siteContent["contact"]["phone"]
+const email=document.querySelector('.contact p:nth-of-type(3)')
+email.textContent=siteContent["contact"]["email"]
+
+const footer=document.querySelector('footer p')
+footer.textContent=siteContent["footer"]["copyright"]
